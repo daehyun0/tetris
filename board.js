03098fb2651b84145baeba3493257782f63b7ca5
daehyunJoo <dd70296@gmail.com>
2020-06-10 17:13:52 +0900

    add function about isTetromino inside wall&floor

--- board.js
+++ board.js
@@ -10,6 +10,21 @@ class Board {
         this.grid = this.getEmptyBoard();
     }
 
+    canMovePiece(piece) {
+        function isXposInsideWall(x) { return  (0 <= x && x < COL_COUNT) }
+        function isYposInsideFloor(y) { return y < ROW_COUNT }
+
+        return piece.shape.every((row, dy) => {
+            return row.every((value, dx) => {
+                let x = piece.x + dx;
+                let y = piece.y + dy;
+
+                return value === 0 ||
+                    (isXposInsideWall(x) && isYposInsideFloor(y));
+            })
+        })
+    }
+
     getEmptyBoard() {
         return Array.from(
             { length: ROW_COUNT }, () => Array(COL_COUNT).fill(0)
--- main.js
+++ main.js
@@ -7,27 +7,30 @@ ctx.canvas.height = ROW_COUNT * BLOCK_SIZE;
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
 
 let board = new Board();
+let piece = new Piece(ctx);
+board.piece = piece;
+
 let moves = {
     [KEY.LEFT]: piece => ({...piece, x: piece.x - 1}),
     [KEY.RIGHT]: piece => ({...piece, x: piece.x + 1}),
     [KEY.DOWN]: piece => ({...piece, y: piece.y + 1})
 }
 
-
 function play() {
     board.reset();
-    let piece = new Piece(ctx);
-    board.piece = piece;
+    board.draw();
 
     document.addEventListener("keydown", event => {
         if (moves[event.key]) {
             event.preventDefault();
 
-            ctx.clearRect(piece.x - 1, piece.y - 1, 3 * BLOCK_SIZE, 3 * BLOCK_SIZE);
-
             let newPiece = moves[event.key](piece);
-            board.piece.move(newPiece);
-            board.draw();
+
+            if (board.canMovePiece(newPiece)) {
+                ctx.clearRect(piece.x - 1, piece.y - 1, 3 * BLOCK_SIZE, 3 * BLOCK_SIZE);
+                board.piece.move(newPiece);
+                board.draw();
+            }
         }
     })
 }
